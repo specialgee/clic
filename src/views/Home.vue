84585f3c43ea1805868edf6d43a71407762cf2df
gee <special.geeee@gmail.com>
2020-01-08 07:04:34 +0100

    updated UI layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,37 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <p id="button-wrapper">
+    <div id="button-wrapper">
       <button id="plus-button" @click="increment">
         <p id="count-text">{{ count }}</p>
       </button>
-      <button @click="decrement">-</button>
-    </p>
+      <button id="minus-button" @click="decrement">BACK</button>
+      <div class="corner-bloc tr"></div>
+      <div class="corner-bloc bl"></div>
+      <div class="corner-bloc br"></div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.tr {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+
+.bl {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+
+.br {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+
 .home {
   position: relative;
   height: 100vh;
@@ -28,7 +49,7 @@ button {
   display: inline-block;
   background-color: #ddd;
   border: none;
-  color: white;
+  color: #333;
   text-align: center;
   text-decoration: none;
   font-size: 16px;
@@ -51,9 +72,26 @@ button:focus {
   transform: translate(-50%, -50%);
 }
 
+#minus-button {
+  width: 50px;
+  height: 50px;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
 #count-text {
   margin: 0;
-  color: #333;
+  font-size: 42px;
+}
+
+.corner-bloc {
+  width: 50px;
+  height: 50px;
+  background-color: #ddd;
+  border: none;
 }
 
 </style>
